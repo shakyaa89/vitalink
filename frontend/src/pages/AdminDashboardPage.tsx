d488776df8eae9180ebfb27988d0a7ba8d38d93d
shakyaa89 <shashwatshakya89@gmail.com>
2025-09-16 23:48:33 +0545

    Updated Admin Dashboard

--- frontend/src/pages/AdminDashboardPage.tsx
+++ frontend/src/pages/AdminDashboardPage.tsx
@@ -1,10 +1,57 @@
-import React from "react";
+import DoctorsList from "../components/DoctorsList";
 import UsersList from "../components/UsersList";
+import { useState } from "react";
+import AdminDashboard from "../components/AdminDashboard";
 
 function AdminDashboardPage() {
+  const [activeSection, setActiveSection] = useState("");
+
   return (
     <>
-      <UsersList />
+      <div className="mt-5 flex items-center justify-around px-5 md:px-30 lg:px-50 box-border">
+        <div className="w-full border-b flex items-center justify-around py-3 md:py-5 md:pb-10 box-border">
+          <button
+            onClick={() => {
+              setActiveSection("dashboard");
+            }}
+            className={
+              activeSection === "dashboard"
+                ? "cursor-pointer border-b-2"
+                : "cursor-pointer"
+            }
+          >
+            Dashboard
+          </button>
+          <button
+            onClick={() => {
+              setActiveSection("users");
+            }}
+            className={
+              activeSection === "users"
+                ? "cursor-pointer border-b-2"
+                : "cursor-pointer"
+            }
+          >
+            All Users
+          </button>
+          <button
+            onClick={() => {
+              setActiveSection("doctors");
+            }}
+            className={
+              activeSection === "doctors"
+                ? "cursor-pointer border-b-2"
+                : "cursor-pointer"
+            }
+          >
+            All Doctors
+          </button>
+        </div>
+      </div>
+
+      {activeSection === "dashboard" && <AdminDashboard />}
+      {activeSection === "users" && <UsersList />}
+      {activeSection === "doctors" && <DoctorsList />}
     </>
   );
 }
